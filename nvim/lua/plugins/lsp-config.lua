01c26628eb9ffa14d94aae2e6575ace7f7b8466a
jadfeitrouni <jadfeitrouni@gmail.com>
2025-01-31 14:39:43 -0500

    Added neotree

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -23,7 +23,7 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.ts_ls.setup({})
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end,
     },
