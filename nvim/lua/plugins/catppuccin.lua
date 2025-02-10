7fddd8d1a7d1dca9fb4099a3f33bd13d19f9cc71
jadfeitrouni <jadfeitrouni@gmail.com>
2025-02-10 09:31:30 -0500

    Resetup lazyvim

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,13 +1,13 @@
 return {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
       flavour = "macchiato",
-      transparent_background = true
+      transparent_background = true,
     })
-        vim.cmd.colorscheme "catppuccin"
-    end,
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
